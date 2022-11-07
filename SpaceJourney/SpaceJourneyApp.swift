693ce9b850a6967a528231e35f8b6d60f944c128
Livsy90 <artmir@live.ru>
2022-11-07 11:27:58 +0400

    Initial Commit

---
+++ SpaceJourney/SpaceJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceJourneyApp.swift
+//  SpaceJourney
+//
+//  Created by Livsy on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
